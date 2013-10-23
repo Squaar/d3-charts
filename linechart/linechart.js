9459a0f79299017d09d7d139223b9b7328f87ded
Matt Dumford <mdumford99@gmail.com>
2013-10-23 18:25:27 -0500

    lines on linechart work

--- linechart/linechart.js
+++ linechart/linechart.js
@@ -1,4 +1,8 @@
-var data = [1, 5, 3, 6, 2];
+var data = [ 
+	[1,2,3],
+	[9,3,1],
+	[2,10,1]
+];
 var dataHeaders = ["a", "b", "c", "d", "e"];
 
 var margin = {top: 30, right: 30, bottom: 30, left:30},
@@ -11,13 +15,23 @@ var svg = d3.select("body").append("svg")
   .append("g")
   	.attr("transform", "translate("+margin.left+","+margin.top+")");
 
+//calculate min and max
+var min = Number.MAX_VALUE;
+var max = Number.MIN_VALUE;
+for(var i=0; i<data.length; i++){
+	if(d3.min(data[i]) < min)
+		min = d3.min(data[i]);
+	if(d3.max(data[i]) > max)
+		max = d3.max(data[i]);
+}
+
 var y = d3.scale.linear()
-	.domain(d3.extent(data))
+	.domain([min, max])
 	.range([height, 0]);
 	
 
 var x = d3.scale.ordinal()
-	.domain(d3.range(data.length))
+	.domain(d3.range(dataHeaders.length))
 	.rangeBands([0, width]);
 
 //horizontal lines
@@ -33,12 +47,12 @@ svg.selectAll("line")
 //function to calculate the x,y points of each line
 var lineFunction = d3.svg.line()
 	.x(function(d,i){ return x(i);})
-	.y(function(d,i){ return y(d);})
+	.y(function(d,i){ return y(d)})
 	.interpolate("cardinal");
 
 //make the paths
 var paths = svg.selectAll(".paths")
 	.data(data)
   .enter().append("path")
-  	.attr("d", function(d){ console.log(d); return lineFunction(d); })
+  	.attr("d", function(d){ console.log(lineFunction(d)); return lineFunction(d); })
   	.attr("fill", "none");
