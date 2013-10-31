9f650784e8b96441ff92363377a9dbb470079df6
Matt Dumford <mdumford99@gmail.com>
2013-10-31 14:55:23 -0500

    added colors, fixed some stuff

--- radarChart/radarchart.js
+++ radarChart/radarchart.js
@@ -1,4 +1,5 @@
 //http://bl.ocks.org/mbostock/3048740
+//http://bl.ocks.org/jeffthink/1630683
 
 
 var dataHeaders = ["colA", "colB", "colC"];
@@ -19,38 +20,52 @@ var svg = d3.select("body").append("svg")
 		.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
 var innerRadius = 0;
-var outerRadius = d3.min([width, height]);
+var outerRadius = d3.min([width/2, height/2]);
 
 var axes = [];
 
 for(var i=0; i<dataHeaders.length; i++){
 	axes[i] = d3.scale.linear()
-		.domain(d3.extent(data[i]))
+		.domain(d3.extent(data, function(j){ return j[i];}))
 		.range([innerRadius, outerRadius]);
 }
+
+var color = d3.scale.category10()
+	.domain(data.length);
 
 var theta = d3.scale.linear()
 		.domain(d3.range(dataHeaders.length))
 		.range([0, 2*Math.PI]);
 
 var x = function(radius, theta){
-	return radius*Math.cos(theta);
+	return radius*Math.cos(theta) + width/2;
 };
 var y = function(radius, theta){
-	return radius*Math.sin(theta);
+	return radius*Math.sin(theta) + height/2;
 };
 
 //function to calculate the x,y points of each line
-var lineFunction = d3.svg.line()
-	.x(function(d,i){ return x(axes[i](d), theta(i));})
-	.y(function(d,i){ return y(axes[i](d), theta(i));})
+// var lineFunction = d3.svg.line()
+// 	.x(function(d,i){ return x(axes[i](d), theta(i));})
+// 	.y(function(d,i){ return y(axes[i](d), theta(i));})
+// 	.interpolate("linear");
+
+var lineFunction = d3.svg.line.radial()
+	.radius(function(d,i){return axes[i](d);})
+	//.angle(function(d,i){return theta(i)})
+	.angle(function(d,i){
+		if(i === dataHeaders.length+1)
+			i=0;
+		return (i/(dataHeaders.length+1)) * 2*Math.PI;})
 	.interpolate("linear");
 
 //make the paths
 var paths = svg.selectAll(".paths")
 	.data(data)
   .enter().append("path")
-  	.attr("d", function(d){ return lineFunction(d); })
+  	.attr("d", function(d,i){ return lineFunction(d,i); })
+  	.attr("stroke", function(d,i){return color(i);})
+  	.attr("transform", "translate(" + width/2 + "," + height/2 + ")")
   	.attr("fill", "none");
 
 // svg.selectAll(".axis")
