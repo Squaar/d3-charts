3819cabab90ea158ede295f6a5a2025e0bf3cefc
Matt Dumford <mdumford99@gmail.com>
2013-11-03 17:35:25 -0600

    axes almost work

--- radarChart/radarchart.js
+++ radarChart/radarchart.js
@@ -2,7 +2,7 @@
 //http://bl.ocks.org/jeffthink/1630683
 
 
-var dataHeaders = ["colA", "colB", "colC","d","e","f"];
+var dataHeaders = ["A", "B", "C","D","E","F"];
 var data = [
 	[1,2,3,4,5,6],
 	[2,4,6,8,10,12],
@@ -34,29 +34,22 @@ for(var i=0; i<dataHeaders.length; i++){
 var color = d3.scale.category10()
 	.domain(data.length);
 
-// var theta = d3.scale.linear()
-// 		.domain(d3.range(dataHeaders.length))
-// 		.range([0, 2*Math.PI]);
+var x = function(radius, theta){
+	return radius*Math.cos(theta) + width/2;
+};
+var y = function(radius, theta){
+	return radius*Math.sin(theta) + height/2;
+};
 
-// var x = function(radius, theta){
-// 	return radius*Math.cos(theta) + width/2;
-// };
-// var y = function(radius, theta){
-// 	return radius*Math.sin(theta) + height/2;
-// };
-
+var theta = function(i){
+	return (i/dataHeaders.length) * 2*Math.PI;
+}
 
 //append first value of each line to end of each line
 //to create a complete loop
 for(var i=0; i<data.length; i++){
 	data[i][data[i].length] = data[i][0];
 }
-
-//function to calculate the x,y points of each line
-// var lineFunction = d3.svg.line()
-// 	.x(function(d,i){ return x(axes[i](d), theta(i));})
-// 	.y(function(d,i){ return y(axes[i](d), theta(i));})
-// 	.interpolate("linear");
 
 var lineFunction = d3.svg.line.radial()
 	.radius(function(d,i){
@@ -65,34 +58,39 @@ var lineFunction = d3.svg.line.radial()
 	//.angle(function(d,i){return theta(i)})
 	.angle(function(d,i){
 		if(i == dataHeaders.length) i=0;
-		return (i/(dataHeaders.length)) * 2*Math.PI;})
+		return (i/dataHeaders.length) * 2*Math.PI;})
 	.interpolate("linear");
 
 //make the paths
 var paths = svg.selectAll(".paths")
 	.data(data)
   .enter().append("path")
+  	.attr("class", "data-path")
   	.attr("d", function(d,i){ return lineFunction(d,i); })
   	.attr("stroke", function(d,i){return color(i);})
   	.attr("transform", "translate(" + width/2 + "," + height/2 + ")")
   	.attr("fill", "none");
 
-// svg.selectAll(".axis")
-// 		.data(dataHeaders)
-// 	.enter().append("g")
-// 		.attr("class", "axis")
-// 		.attr("transform", function(d,i) { return "translate(" + x(width/2 + innerRadius, theta(i)) + "," + y(height/2 + innerRadius, theta(i)) + ")rotate(" + theta(i) * 180 / Math.PI + ")"; })
-// 		//.attr("transform", function(d,i) { return "rotate(" + theta(i) * 180 / Math.PI + ")"; })
-// 		//.attr("transform", function(d,i) { return "translate(" + x(width/2 + innerRadius, theta(i)) + "," + y(height/2 + innerRadius, theta(i)) + ")";})
-// 	.each(function(d,i){ d3.select(this).call(d3.svg.axis()
-// 		.orient("left")
-// 		.scale(axes[i]));})
-// 	.append("text")
-// 		.attr("y", -outerRadius+6)
-// 		.attr("text-anchor", "middle")
-// 		.text(String);
+//make the axes
+svg.selectAll(".axis")
+		.data(dataHeaders)
+	.enter().append("g")
+		.attr("class", "axis")
+		.attr("transform", function(d,i) { 
+			return "translate(" + x(innerRadius, theta(i)) + "," + y(innerRadius, theta(i)) + 
+			")rotate(" + theta(i) * 180 / Math.PI + ")"; })
+		//.attr("transform", function(d,i) { return "rotate(" + theta(i) * 180 / Math.PI + ")"; })
+		//.attr("transform", function(d,i) { return "translate(" + x(width/2 + innerRadius, theta(i)) + "," + y(height/2 + innerRadius, theta(i)) + ")";})
+	.each(function(d,i){ d3.select(this).call(d3.svg.axis()
+		.orient("left")
+		.scale(axes[i]));})
+	.append("text")
+		.attr("y", -outerRadius+6)
+		.attr("text-anchor", "middle")
+		.text(String);
 
 
+//REMEMBER TO REMOVE CENTER RECT
 svg.append("rect")
 	.attr("x", width/2 - 2)
 	.attr("y", height/2 -2)
